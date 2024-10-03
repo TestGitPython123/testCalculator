c81c344bc1056077b778927ff5fbeed0937d9e59
Ginsburg_Vladislav Laptop <PaddeusVladislav@gmail.com>
2024-10-03 18:53:31 +0400

    Исправлена ошибка деления на ноль

--- Main.py
+++ Main.py
@@ -12,8 +12,11 @@ elif operation == "-":
     print(f"{first_value} - {second_value} "
           f"= {first_value - second_value}")
 elif operation == "/":
-    print(f"{first_value} / {second_value} "
-          f"= {first_value / second_value}")
+    if first_value == 0 or second_value == 0:
+        print("Делить на 0 нельзя")
+    else:
+        print(f"{first_value} / {second_value} "
+            f"= {first_value / second_value}")
 elif operation == "*":
     print(f"{first_value} * {second_value} "
           f"= {first_value / second_value}")
