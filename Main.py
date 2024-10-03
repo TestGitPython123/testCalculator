7e07c0f0700d6c034ff5d21fc20460e76232a1e1
Ginsburg_Vladislav Laptop <PaddeusVladislav@gmail.com>
2024-10-03 18:50:25 +0400

    Добавлен минимальный функционал и операция сложения

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
 print("Добро пожаловать в калькулятор")
+
+first_value = float(input("Введите первое число: "))
+second_value = float(input("Введите второе число: "))
+
+operation = input("Введите символ операции: ")
+
+if operation == "+":
+    print(f"{first_value} + {second_value} "
+          f"= {first_value + second_value}")
